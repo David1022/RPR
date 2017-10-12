8ef928c9f7806c72bb7982e0a61e1e94f3ecbef7
david <dav_mm@hotmail.com>
2017-10-12 12:39:26 +0200

    Added jxl.jar library

---
+++ app/src/main/java/com/nipsa/rpr/ExcelHandler.java
@@ -0,0 +1,13 @@
+package com.nipsa.rpr;
+
+import jxl.WorkbookSettings;
+
+public class ExcelHandler {
+
+    public ExcelHandler() {
+    }
+
+    public void generateExcelFile() {
+        WorkbookSettings wbSettings = new WorkbookSettings();
+    }
+}
