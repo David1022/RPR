9f1320093474bf2a12478d4a60168096e495374b
David <dmendanyo@gmail.com>
2017-11-24 09:19:14 +0100

    Added exceptions throws for ExcelReader

--- app/src/main/java/com/nipsa/rpr/ExcelHandler/ExcelReader.java
+++ app/src/main/java/com/nipsa/rpr/ExcelHandler/ExcelReader.java
@@ -27,6 +27,8 @@ public class ExcelReader {
     public static final int SECOND_SHEET_NUM_COLUMNS = 17;
     public static final int THIRD_SHEET_NUM_COLUMNS = 2;
     public static final String SPACE = " ";
+    public static final int REVISION_NAME_COL = 0;
+    public static final int REVISION_NAME_ROW = 2;
 
     private File mExcelToRead;
     private Context mContext;
@@ -49,22 +51,33 @@ public class ExcelReader {
             Log.e(Aplicacion.TAG, "ExcelReader.readExcelFile: " + ioe.toString());
         } catch (BiffException be) {
             Log.e(Aplicacion.TAG, "ExcelReader.readExcelFile: " + be.toString());
+        } catch (IllegalArgumentException iae) { // Exception caused by the expected excel file format
+            Log.e(Aplicacion.TAG, "Error en el formato del excel" + iae.toString());
+            throw new IllegalArgumentException(iae.toString());
         }
     }
 
-    private void readFirstSheet(Sheet sheet) {
+    private void readFirstSheet(Sheet sheet) throws IllegalArgumentException {
         Vector<String> rowData = new Vector<>();
         int totalRows = sheet.getRows();
-        int totalCol = FIRST_SHEET_NUM_COLUMNS;
+        int totalCol = sheet.getColumns();
 
         readRevisionName(sheet);
 
-        for (int row = MARK_FIRST_SHEET_INIT_ROW; row < totalRows; row++) { // get all the rows
-            rowData.clear();
-            for (int col = 0; col < totalCol; col++) { // for each rows, get all the columns (cells)
-                rowData.add(sheet.getCell(col, row).getContents());
+        if ((totalRows - MARK_FIRST_SHEET_INIT_ROW) > 0) {
+            if ((totalCol - FIRST_SHEET_NUM_COLUMNS) < 0) {
+                for (int row = MARK_FIRST_SHEET_INIT_ROW; row < totalRows; row++) { // get all the rows
+                    rowData.clear();
+                    for (int col = 0; col < FIRST_SHEET_NUM_COLUMNS; col++) { // for each row, get all the columns (cells)
+                        rowData.add(sheet.getCell(col, row).getContents());
+                    }
+                    mDBRevisiones.incluirEquipo(rowData);
+                }
+            } else {
+                throw new IllegalArgumentException(("Illegal number of columns in sheet 1"));
             }
-            mDBRevisiones.incluirEquipo(rowData);
+        } else {
+            throw new IllegalArgumentException("Illegal number of rows in sheet 1");
         }
 
     }
@@ -72,42 +85,57 @@ public class ExcelReader {
     private void readSecondSheet(Sheet sheet) {
         Vector<String> rowData = new Vector<>();
         int totalRows = sheet.getRows();
-        int totalCol = SECOND_SHEET_NUM_COLUMNS;
+        int totalCol = sheet.getColumns();
 
-        // TODO: Comprobar si lee y guarda bien los apoyos
-
-        for (int row = MARK_SECOND_SHEET_INIT_ROW; row < totalRows; row++) { // get all the rows
-            rowData.clear();
-            for (int col = 1; col < totalCol; col++) { // for each rows, get all the columns (cells)
-                rowData.add(sheet.getCell(col, row).getContents());
+        // TODO: Hacer comprobaciones para los casos en que los excel estén mal formados
+        if ((totalRows - MARK_SECOND_SHEET_INIT_ROW) > 0) {
+            if ((totalCol - SECOND_SHEET_NUM_COLUMNS) < 0) {
+                for (int row = MARK_SECOND_SHEET_INIT_ROW; row < totalRows; row++) { // get all the rows
+                    rowData.clear();
+                    for (int col = 1; col < SECOND_SHEET_NUM_COLUMNS; col++) { // for each row, get all the columns (cells)
+                        rowData.add(sheet.getCell(col, row).getContents());
+                    }
+                    mDBRevisiones.incluirApoyo(rowData);
+                }
+            } else {
+                throw new IllegalArgumentException(("Illegal number of columns in sheet 2"));
             }
-            mDBRevisiones.incluirApoyo(rowData);
+        } else {
+            throw new IllegalArgumentException("Illegal number of rows in sheet 2");
         }
-
     }
 
     private void readThirdSheet(Sheet sheet) {
         Vector<String> rowData = new Vector<>();
         int totalRows = sheet.getRows();
-        int totalCol = THIRD_SHEET_NUM_COLUMNS;
+        int totalCol = sheet.getColumns();
 
-        // TODO: Comprobar si lee y guarda bien los apoyos
-
-        for (int row = MARK_THIRD_SHEET_INIT_ROW; row < totalRows; row++) { // get all the rows
-            rowData.clear();
-            for (int col = 0; col < totalCol; col++) { // for each rows, get all the columns (cells)
-                rowData.add(sheet.getCell(col, row).getContents());
+        // TODO: Hacer comprobaciones para los casos en que los excel estén mal formados
+        if ((totalRows - MARK_THIRD_SHEET_INIT_ROW) >= 0) {
+            if ((totalCol - THIRD_SHEET_NUM_COLUMNS) < 0) {
+                for (int row = MARK_THIRD_SHEET_INIT_ROW; row < totalRows; row++) { // get all the rows
+                    rowData.clear();
+                    for (int col = 0; col < THIRD_SHEET_NUM_COLUMNS; col++) { // for each row, get all the columns (cells)
+                        rowData.add(sheet.getCell(col, row).getContents());
+                    }
+                    mDBRevisiones.incluirApoyoNoRevisable(rowData);
+                }
+            } else {
+                throw new IllegalArgumentException(("Illegal number of columns in sheet 3"));
             }
-            mDBRevisiones.incluirApoyoNoRevisable(rowData);
+        } else {
+            throw new IllegalArgumentException("Illegal number of rows in sheet 3");
         }
 
     }
 
-    private void readRevisionName(Sheet sheet) {
-        String revisionName = sheet.getCell(2, 0).getContents();
+    private void readRevisionName(Sheet sheet) throws IllegalArgumentException {
+        String revisionName = sheet.getCell(REVISION_NAME_COL, REVISION_NAME_ROW).getContents();
 
-        if (revisionName != null) {
+        if (revisionName != null && revisionName.contains((SPACE))) {
             revisionName = revisionName.substring(revisionName.lastIndexOf(SPACE) + 1);
+        } else {
+            throw new IllegalArgumentException("Error in the title cell");
         }
 
         Aplicacion.tituloRevision = revisionName;
